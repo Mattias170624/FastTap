f3e5b61f2743fd6960596318f7c80a5df13fede2
Mattias <mattias170624@gmail.com>
2022-01-16 14:37:21 +0100

    Initial Commit

---
+++ FastTap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastTap
+//
+//  Created by Mattias Andersson on 2022-01-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastTap/FastTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTapApp.swift
+//  FastTap
+//
+//  Created by Mattias Andersson on 2022-01-16.
+//
+
+import SwiftUI
+
+@main
+struct FastTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
