7e6aae078115a954baf137274f2ceedfd21dfa4b
Mattias <mattias170624@gmail.com>
2022-01-16 19:02:49 +0100

    Login screen without firestore

--- FastTap/ContentView.swift
+++ FastTap/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var nickname: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Text("Fast \n    Tap")
+                .fontWeight(.bold)
+                .font(.largeTitle)
+                .frame(height: 400)
+            
+            TextField("Nickname...", text: $nickname)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .disableAutocorrection(true)
+                .frame(width: 300, height: 50, alignment: .center)
+            
+            SecureField("Password...", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 300, height: 50, alignment: .center)
+            
+            Button(action: {
+                registerButton()
+            }, label: {
+                Text("Don't have a account? \n Register here")
+                    .font(.caption)
+                    .padding()
+            })
+            
+            Button(action: {
+                loginButton()
+            }, label: {
+                Text("Login")
+                    .font(.title)
+                    .fontWeight(.bold)
+            })
+                .buttonStyle(.bordered)
+                .padding(.top)
+            
+        }
+    }
+    
+    private func registerButton() {
+        print("Register button tapped")
+    }
+    
+    private func loginButton() {
+        print("Nickname: \(nickname)")
+        print("Password: \(password)")
+        
+        
+        
     }
 }
 
