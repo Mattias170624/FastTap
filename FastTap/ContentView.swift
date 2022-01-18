d777f350292224544b89adcf582a51014dc80461
Mattias <mattias170624@gmail.com>
2022-01-18 23:19:34 +0100

    Added login and register screen with firebase

--- FastTap/ContentView.swift
+++ FastTap/ContentView.swift
@@ -6,10 +6,100 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
 
 struct ContentView: View {
-    @State private var nickname: String = ""
+    var db = Firestore.firestore()
+    let firebaseAuth = Auth.auth()
+    
+    @State private var email: String = ""
     @State private var password: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                Text("Fast \n    Tap")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                
+                Spacer()
+                    .frame(height: 150)
+                
+                HStack {
+                    Image(systemName: "envelope")
+                        .foregroundColor(.gray)
+                    
+                    TextField("Email", text: $email)
+                        .disableAutocorrection(true)
+                }
+                .padding(.horizontal)
+                .frame(width: 300.0, height: 50.0)
+                .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray))
+                
+                Spacer()
+                    .frame(height: 10)
+                
+                HStack {
+                    Image(systemName: "lock")
+                        .foregroundColor(.gray)
+                    
+                    SecureField("Password", text: $password)
+                        .disableAutocorrection(true)
+                }
+                .padding(.horizontal)
+                .frame(width: 300.0, height: 50.0)
+                .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray))
+                NavigationLink(destination: RegisterView(), label: {
+                    Text("Create an account here")
+                })
+                
+                Button(action: {
+                    loginButton()
+                }, label: {
+                    Text("Login")
+                        .frame(width: 250, height: 50, alignment: .center)
+                        .font(.title)
+                })
+                    .buttonStyle(.bordered)
+                    .padding(.top, 75)
+            }
+        }
+    }
+    
+    private func loginButton() {
+        if email == "" && password == "" {
+            print("Fill in email and password")
+        } else {
+            
+            firebaseAuth.signIn(withEmail: email, password: password) { (result, error) in
+                if result == nil {
+                    print("Failed to login")
+                    // show message on what failed
+                } else {
+                    print("Sucess loggin in")
+                    // transfer over to mainscreen
+                }
+            }
+        }
+    }
+}
+
+
+
+
+struct RegisterView: View {
+    var db = Firestore.firestore()
+    let firebaseAuth = Auth.auth()
+    
+    @State private var nickname: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var showingSheet: Bool = false
+    var agreedToTerms: Bool = false
     
     var body: some View {
         VStack {
@@ -17,48 +107,110 @@ struct ContentView: View {
             Text("Fast \n    Tap")
                 .fontWeight(.bold)
                 .font(.largeTitle)
-                .frame(height: 400)
+                .navigationTitle("Register").navigationBarTitleDisplayMode(.inline)
             
-            TextField("Nickname...", text: $nickname)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .disableAutocorrection(true)
-                .frame(width: 300, height: 50, alignment: .center)
+            Spacer()
+                .frame(height: 150)
             
-            SecureField("Password...", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .frame(width: 300, height: 50, alignment: .center)
+            HStack {
+                Image(systemName: "person")
+                    .foregroundColor(.gray)
+                
+                TextField("Nickname", text: $nickname)
+                    .disableAutocorrection(true)
+                
+            }
+            .padding(.horizontal)
+            .frame(width: 300.0, height: 50.0)
+            .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray))
             
-            Button(action: {
-                registerButton()
-            }, label: {
-                Text("Don't have a account? \n Register here")
-                    .font(.caption)
-                    .padding()
+            Spacer()
+                .frame(height: 10)
+            
+            HStack {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                
+                TextField("Email", text: $email)
+                    .disableAutocorrection(true)
+            }
+            .padding(.horizontal)
+            .frame(width: 300.0, height: 50.0)
+            .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray))
+            
+            Spacer()
+                .frame(height: 10)
+            
+            HStack {
+                Image(systemName: "lock")
+                    .foregroundColor(.gray)
+                
+                SecureField("Password", text: $password)
+                    .disableAutocorrection(true)
+            }
+            .padding(.horizontal)
+            .frame(width: 300.0, height: 50.0)
+            .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray))
+            
+            HStack {
+                Button(action: {
+                    showingSheet.toggle()
+                }, label: {
+                    Text("By creating an account you agree \n to the terms & conditions")
+                        .font(.body)
+                        .italic()
+                })
+            }
+            .sheet(isPresented: $showingSheet, content: {
+                VStack {
+                    Text("License Agreement")
+                        .font(.title)
+                        .padding(50)
+                    
+                    Spacer()
+                    
+                    Text("1: Everyone can add you to their friendlist \nwhen you create an account")
+                        .multilineTextAlignment(.leading)
+                        .font(.body)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        showingSheet.toggle()
+                    }, label: {
+                        Text("Dismiss")
+                    })
+                        .padding(.bottom, 50)
+                }
             })
             
             Button(action: {
-                loginButton()
+                registerAccount()
             }, label: {
-                Text("Login")
+                Text("Register")
+                    .frame(width: 250, height: 50, alignment: .center)
                     .font(.title)
-                    .fontWeight(.bold)
             })
                 .buttonStyle(.bordered)
-                .padding(.top)
-            
+                .padding(.top, 75)
         }
     }
     
-    private func registerButton() {
-        print("Register button tapped")
-    }
-    
-    private func loginButton() {
-        print("Nickname: \(nickname)")
-        print("Password: \(password)")
-        
-        
-        
+    func registerAccount() {
+        if nickname == "" || email == "" || password == "" {
+            print("Fill in all fields")
+        } else {
+            firebaseAuth.createUser(withEmail: email, password: password) { (result, error) in
+                if result == nil {
+                    print("Failed to create an account")
+                } else {
+                    print("Sucess creating an account")
+                }
+            }
+        }
     }
 }
 
