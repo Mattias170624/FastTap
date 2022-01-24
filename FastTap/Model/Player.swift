e3d5f2ba29ed30c12fc6af8940facfdb26e1f5ef
Mattias <mattias170624@gmail.com>
2022-01-24 19:54:53 +0100

    Created seperate files for data handling

---
+++ FastTap/Model/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  FastTap
+//
+//  Created by Mattias Andersson on 2022-01-21.
+//
+
+import Foundation
---
+++ FastTap/View/Homescreen.swift
@@ -0,0 +1,20 @@
+//
+//  Homescreen.swift
+//  FastTap
+//
+//  Created by Mattias Andersson on 2022-01-24.
+//
+
+import SwiftUI
+
+struct Homescreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Homescreen_Previews: PreviewProvider {
+    static var previews: some View {
+        Homescreen()
+    }
+}
