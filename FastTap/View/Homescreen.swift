c3b2712d18f441f93990e6bcb5072386dc893e2e
Mattias <mattias170624@gmail.com>
2022-01-27 14:18:00 +0100

    Added tabview and made homescreen primary screen when logging in

--- FastTap/View/Homescreen.swift
+++ FastTap/View/Homescreen.swift
@@ -12,48 +12,76 @@ import FirebaseAuth
 struct Homescreen: View {
     let firebaseAuth = Auth.auth()
     @State private var backToStartScreen: Bool = false
-
+    @State private var defaultTabView = 3
+    
     
     var body: some View {
-        VStack {
-            Text("Fast \n    Tap")
-                .fontWeight(.bold)
-                .font(.largeTitle)
+        TabView(selection: $defaultTabView) {
             
-            Spacer()
-                .frame(height: 50)
-            
-            Text("Welcome back")
-                .font(.headline)
-            
-            Text("\(Player.nickname)!")
-                .italic()
-            
-            Spacer()
-                .frame(height: 150)
-            
-            Text("Practice score:")
-            Text("\(Player.onlineScore)")
-                .foregroundColor(.green)
-            
-            Text("Multiplayer score:")
-            Text("X")
-                .foregroundColor(.green)
-            
-            //  Temporary signout button
-            Button(action: {
-                do {
-                    try firebaseAuth.signOut()
-                    backToStartScreen.toggle()
-                } catch {
-                    print("Error signing out")
+            Text("Settings Tab")
+                .tabItem {
+                    Label("Settings", systemImage: "gearshape")
                 }
-            }, label: {
-                Text("sign out")
-            })
-                .fullScreenCover(isPresented: $backToStartScreen, content: {
-                ContentView()
-            })
+            
+            Text("Score Tab")
+                .tabItem {
+                    Label("Score", systemImage: "list.bullet")
+                }
+            
+            VStack {
+                Text("Fast \n    Tap")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                
+                Spacer()
+                    .frame(height: 50)
+                
+                Text("Welcome back")
+                    .font(.headline)
+                
+                Text("\(Player.nickname)!")
+                    .italic()
+                
+                Spacer()
+                    .frame(height: 150)
+                
+                Text("Practice score:")
+                Text("\(Player.onlineScore)")
+                    .foregroundColor(.green)
+                
+                Text("Multiplayer score:")
+                Text("X")
+                    .foregroundColor(.green)
+                
+                //  Temporary signout button
+                Button(action: {
+                    do {
+                        try firebaseAuth.signOut()
+                        backToStartScreen.toggle()
+                    } catch {
+                        print("Error signing out")
+                    }
+                }, label: {
+                    Text("sign out")
+                })
+                    .fullScreenCover(isPresented: $backToStartScreen, content: {
+                        ContentView()
+                    })
+            }
+            .tabItem {
+                Label("Home", systemImage: "house")
+            }
+            .tag(3)
+            
+            Text("The Third Tab")
+                .tabItem {
+                    Label("Play", systemImage: "play.circle")
+                }
+            
+            Text("The Third Tab")
+                .tabItem {
+                    Label("Players", systemImage: "person.crop.circle.badge.plus")
+                }
         }
     }
 }
