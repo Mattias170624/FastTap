4b9fb62c2cb32a692796158c964aa4069ef75698
Mattias <mattias170624@gmail.com>
2022-01-27 18:25:15 +0100

    Added static elements to all tab views

--- FastTap/View/Homescreen.swift
+++ FastTap/View/Homescreen.swift
@@ -13,20 +13,63 @@ struct Homescreen: View {
     let firebaseAuth = Auth.auth()
     @State private var backToStartScreen: Bool = false
     @State private var defaultTabView = 3
-    
+    @State private var showingGameScreen: Bool = false
+    @State private var nicknameSearch: String = ""
+    @State var darkTheme: Bool = false
     
     var body: some View {
         TabView(selection: $defaultTabView) {
             
-            Text("Settings Tab")
-                .tabItem {
-                    Label("Settings", systemImage: "gearshape")
+            VStack {
+                Text("Settings")
+                    .font(.title2)
+                    .padding()
+                
+                Form {
+                    Section(header: Text("Themes")) {
+                        Toggle(isOn: $darkTheme, label: {
+                            Text("Dark theme")
+                        })
+                    }
+                    
+                    Section(header: Text("Profile")) {
+                        Button(action: {
+                            do {
+                                try firebaseAuth.signOut()
+                                backToStartScreen.toggle()
+                            } catch {
+                                print("Error signing out")
+                            }
+                        }, label: {
+                            Text("Sign out")
+                                .foregroundColor(Color(.systemRed))
+                        })
+                            .fullScreenCover(isPresented: $backToStartScreen, content: {
+                                ContentView()
+                            })
+                    }
                 }
+                Spacer()
+            }
+            .tabItem {
+                Label("Settings", systemImage: "gearshape")
+            }
             
-            Text("Score Tab")
-                .tabItem {
-                    Label("Score", systemImage: "list.bullet")
+            VStack {
+                Text("Score")
+                    .font(.title2)
+                    .padding()
+                
+                List {
+                    Text("To be added")
                 }
+                
+                Spacer()
+                
+            }
+            .tabItem {
+                Label("Score", systemImage: "list.bullet")
+            }
             
             VStack {
                 Text("Fast \n    Tap")
@@ -36,52 +79,148 @@ struct Homescreen: View {
                 Spacer()
                     .frame(height: 50)
                 
-                Text("Welcome back")
-                    .font(.headline)
-                
-                Text("\(Player.nickname)!")
-                    .italic()
+                Text("Welcome back!\n\(Player.nickname)")
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 200, height: 100)
+                    .background(Color(.systemGroupedBackground))
+                    .cornerRadius(10)
                 
                 Spacer()
-                    .frame(height: 150)
+                    .frame(height: 250)
                 
-                Text("Practice score:")
-                Text("\(Player.onlineScore)")
-                    .foregroundColor(.green)
-                
-                Text("Multiplayer score:")
-                Text("X")
-                    .foregroundColor(.green)
-                
-                //  Temporary signout button
-                Button(action: {
-                    do {
-                        try firebaseAuth.signOut()
-                        backToStartScreen.toggle()
-                    } catch {
-                        print("Error signing out")
+                HStack {
+                    VStack {
+                        Text("Practice score")
+                            .font(.title3)
+                        Text("0")
+                            .foregroundColor(.green)
+                            .font(.title2)
+                            .bold()
                     }
-                }, label: {
-                    Text("sign out")
-                })
-                    .fullScreenCover(isPresented: $backToStartScreen, content: {
-                        ContentView()
-                    })
+                    .padding()
+                    .background(Color(.systemGroupedBackground))
+                    .cornerRadius(10)
+                    
+                    VStack {
+                        Text("Online score")
+                            .font(.title3)
+                        Text("\(Player.onlineScore)")
+                            .foregroundColor(.green)
+                            .font(.title2)
+                            .bold()
+                    }
+                    .padding()
+                    .background(Color(.systemGroupedBackground))
+                    .cornerRadius(10)
+                }
             }
             .tabItem {
                 Label("Home", systemImage: "house")
             }
             .tag(3)
             
-            Text("The Third Tab")
-                .tabItem {
-                    Label("Play", systemImage: "play.circle")
+            VStack {
+                Text("Play")
+                    .font(.title2)
+                    .padding()
+                
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    Button("Practice mode") {
+                        showingGameScreen.toggle()
+                    }
+                    .padding()
+                    .font(.title3)
+                    .foregroundColor(.white)
+                    .background(Color(.systemGreen))
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                    
+                    Text("In practice mode your highscore will be saved locally and will not be seen by other players")
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .frame(width: 300)
                 }
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(10)
+                
+                Spacer()
+                    .frame(height: 25)
+                
+                VStack(spacing: 20) {
+                    Button("Multiplayer mode") {
+                        showingGameScreen.toggle()
+                    }
+                    .padding()
+                    .font(.title3)
+                    .foregroundColor(.white)
+                    .background(Color(.systemGreen))
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                    
+                    Text("In multiplayer mode your highscore will be seen by your friends, but you can only play 3 times / day")
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .frame(width: 300)
+                    
+                    Text("Entries left today:")
+                        .italic()
+                    
+                    Text("X")
+                        .foregroundColor(Color(.systemRed))
+                        .bold()
+                }
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(10)
+                
+                Spacer()
+            }
+            .tabItem {
+                Label("Play", systemImage: "play.circle")
+            }
             
-            Text("The Third Tab")
-                .tabItem {
-                    Label("Players", systemImage: "person.crop.circle.badge.plus")
+            VStack {
+                Text("Friends")
+                    .font(.title2)
+                    .padding()
+                
+                Spacer()
+                
+                Text("You can find a friend by searching\nfor their nickname down below")
+                    .padding()
+                    .background(Color(.systemGroupedBackground))
+                    .cornerRadius(10)
+                
+                Spacer()
+                    .frame(height: 50)
+                
+                HStack {
+                    Image(systemName: "person")
+                        .foregroundColor(.gray)
+                    
+                    TextField("Nickname", text: $nicknameSearch)
+                        .disableAutocorrection(true)
+                        .textInputAutocapitalization(.never)
+                    
+                    Button(action: {
+                        print("Added: \(nicknameSearch)")
+                    }, label: {
+                        Text("Add")
+                            .foregroundColor(Color(.systemGreen))
+                    })
                 }
+                .padding(.horizontal)
+                .frame(width: 300.0, height: 50.0)
+                .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray))
+                
+                Spacer()
+            }
+            .tabItem {
+                Label("Friends", systemImage: "person.crop.circle.badge.plus")
+            }
         }
     }
 }
